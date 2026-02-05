4fd8dc9f2276826cf3ff9e3f61b68e911d363ac0
Aleksandr Hakobyan <ahakobyan@wpi.edu>
2026-02-05 18:46:07 -0500

    Fixing some architecture

---
+++ src/orbit_generators/Validation/validation.m
@@ -0,0 +1,53 @@
+clear; close; clc;
+%%
+addpath(genpath('..\Codebase'))
+
+mu = 0.0121505;
+LP = 'l2';
+L = cr3bp_sys_lagpoints(mu, LP);
+
+LP_Jac = cr3bp_sys_jacconst([L(1), 0, 0, 0, 0, 0], mu);
+
+C_target = linspace(LP_Jac - 0.01,LP_Jac - 0.22,10);
+% C_target = 2.91743;
+%% Lyapunov Orbit Generator
+for i=1:length(C_target)
+    [X0,T_half,JC,nu] = gen_lyapunov(C_target(i),LP,mu);
+    % [X0,T_half,JC] = gen_halo(C_target(i),LP,mu);
+    X0f(i,:) = X0;
+    T_fam(i,1) = 2*T_half;
+    nu_fam(i,1) = nu;
+end
+%% Plotting
+
+a = 3.844 * 1e8;
+m1 = 5.97219*1e24;
+
+figure(1);
+l1p_str = sprintf('L_2 Lyapunov (JC=%.4f)',JC);
+
+opts = odeset('RelTol', 1e-13, 'AbsTol', 1e-13);
+for i = 1:length(C_target)
+    try
+        [t, X] = ode89(@(t,X) cr3bp_eom_gen(t,X,mu), 0:0.0001:T_fam(i), X0f(i,:)');
+        X_In = cr3bp_conv_rot2inert(X,t);
+        [Xdim, tdim] = cr3bp_conv_dim(X,t,a, 'Primary Mass', [m1, mu]);
+    catch
+        fprintf("Not possible to resolve\n");
+    end
+    p1=plot3(Xdim(:,1), Xdim(:,2), Xdim(:,3),'LineWidth',1.5,'DisplayName',l1p_str);
+    axis("equal")
+end
+hold off
+xlabel('x'); ylabel('y');
+title('Planar Lyapunov Orbit [Rotating]');
+
+figure(2); hold on
+plot(C_target,nu_fam,'DisplayName','Vertical Stability Index')
+yline(1,'DisplayName','Bifurcation Point')
+xlabel("Jacobi Constant")
+ylabel("Vertical Stability Index \nu")
+legend('Location','northeast')
+title("Jacobi Constant vs Vertical Stability Index")
+
+
